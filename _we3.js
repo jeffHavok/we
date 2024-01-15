4b7cb0214b9d69c985a819c5c78f6b7c57119fb4
space8ar <cherkashin.dmitryv@gmail.com>
2024-01-15 06:08:56 +0200

    Working inputs & validation

--- _we3.js
+++ _we3.js
@@ -24,9 +24,13 @@ let win2 = {
     params: {
         h: 150,
         w: 178,
+        minh: 120,
+        maxh: 220,
+        minw: 150,
+        maxw: 240,
         col: [40, 60],
         row: [100],
-        colOrder: [0,1,-1],
+        colOrder: [-1,1,0],
         rowOrder: [-1,0]
     },
     pd: [2,2,2,2],
@@ -43,7 +47,6 @@ let win2 = {
             borderless: true,
             child: [
                 {
-                    size: "col-0",
                     pd: [3,3,3,3],
                     child: [{glass: true}],
                 },
@@ -53,6 +56,7 @@ let win2 = {
                     child: [{glass: true}],
                 },
                 {
+                    size: "col-0",
                     pd: [3,3,3,3],
                     child: [{glass: true}],
                 },
@@ -62,15 +66,18 @@ let win2 = {
 }
 
 class WE{
-    constructor(rootId, node, dpi = 2, helpersSize = 20){
+    constructor(rootId, node, dpi = 2, helpersSize = 18){
         this.helpersSize = helpersSize; 
         this.helpers = [];
         this.node = node;
         this.dpi = dpi; 
         this.root = document.getElementById(rootId);
         this.canvas = document.createElement("canvas");
-        this.hEl = document.createElement("input");
+        this.hEl = document.createElement("input");     
         this.wEl = document.createElement("input");
+        this.tip = document.createElement("span");
+        this.colEl = [];
+        this.rowEl = []; 
         this.ctx = null;
     }
 
@@ -171,7 +178,7 @@ class WE{
                     if (i == parent.child.length - 1)
                         node.geom.y2 -= parent.pd[2]; 
                     //---gap
-                    if (i > 0)
+                    if (i > 0) 
                         node.geom.y1 += parent.gap / 2;
                     if (i < parent.child.length - 1)
                         node.geom.y2 -= parent.gap / 2;
@@ -207,7 +214,10 @@ class WE{
     
     drawHelpers(){
         this.ctx.strokeStyle = "#000";
-        this.ctx.lineWidth = this.dpi * 2; 
+        this.ctx.fillStyle = "#000";
+        this.ctx.lineWidth = this.dpi * 1.5; 
+        this.ctx.font = `bold ${this.dpi * 24}px sans-serif`;
+        this.ctx.textAlign = "center";
         this.ctx.beginPath();
 
         let multiplier = 1; 
@@ -237,6 +247,11 @@ class WE{
                 this.ctx.lineTo((helper.pos + helper.value) * this.node.factor, this.node.geom.y1 - this.helpersSize * 0.95);
                 this.ctx.moveTo((helper.pos) * this.node.factor, this.node.geom.y1 - this.helpersSize * 0.85);
                 this.ctx.lineTo((helper.pos + helper.value) * this.node.factor, this.node.geom.y1 - this.helpersSize * 0.85);
+                if (helper.type == "text"){
+                    let textX = (helper.pos + helper.value / 2) * this.node.factor; 
+                    let textY = this.node.geom.y1 - this.helpersSize / 2 + (this.node.factor * 4);  
+                    this.ctx.fillText(helper.value, textX, textY); 
+                }
             }
             if (helper.dir == "row"){
                 this.ctx.moveTo(this.node.geom.x2, helper.pos * this.node.factor);
@@ -245,11 +260,153 @@ class WE{
                 this.ctx.lineTo(this.node.geom.x2 + this.helpersSize * 0.95, (helper.pos + helper.value) * this.node.factor);
                 this.ctx.moveTo(this.node.geom.x2 + this.helpersSize * 0.85, (helper.pos) * this.node.factor);
                 this.ctx.lineTo(this.node.geom.x2 + this.helpersSize * 0.85, (helper.pos + helper.value) * this.node.factor);
+                if (helper.type == "text"){
+                    let textX = this.node.geom.x2 + this.helpersSize / 2.4;
+                    let textY = (helper.pos + helper.value / 2) * this.node.factor + (this.node.factor * 4); 
+                    this.ctx.fillText(helper.value, textX, textY); 
+                }
             }
         })
 
         this.ctx.closePath(); 
         this.ctx.stroke();
+    }
+
+    setRelief(el = this.hEl){
+        el.classList.remove("urgent");
+        this.tip.style.display = "none";
+    }
+
+    setUrgent(el = this.hEl, type = "min", value = 100500){
+        let msgStr = "";
+        el.classList.add("urgent"); 
+        this.tip.style.top = el.style.top;
+        this.tip.style.left = el.style.left;
+        this.tip.style.display = "block";
+        type == "min"
+            ? msgStr += "Min. "
+            : msgStr += "Max. "
+        msgStr += value; 
+        this.tip.innerHTML = msgStr;
+    }
+
+    validate(el, value, min, max){
+        min = (min || 10);
+        max = (max || 1000);
+        let res = 0; 
+        if (value <= max && value >= min){
+            res = value;
+        }
+        else if (value < min){
+            this.setUrgent(el, "min", min)
+            res = min;
+        }
+        else if (value > max){
+            this.setUrgent(el, "max", max)
+            res = max;
+        }
+        res = parseInt(res); 
+        el.value = res; 
+        return(res); 
+    }
+
+    setParam(param = "height", value = 100){
+        if (param == "height"){
+            this.node.params.h = this.validate( this.hEl, value, 
+                                            this.node.params.minh, 
+                                            this.node.params.maxh);
+        } 
+        else if (param == "width"){
+            this.node.params.w = this.validate( this.wEl, value, 
+                                            this.node.params.minw, 
+                                            this.node.params.maxw);
+        }
+        else{
+            let dir = param.split("-")[0];
+            let index = param.split("-")[1]; 
+            if (dir == "col"){
+                this.node.params.col[index] = this.validate( this.colEl[index], value, 
+                                                this.node.params.minCol[index], 
+                                                this.node.params.maxCol[index]);
+            }
+            if (dir == "row"){
+                this.node.params.row[index] = this.validate( this.rowEl[index], value, 
+                                                this.node.params.minRow[index], 
+                                                this.node.params.maxRow[index]);
+            }
+        }
+        // console.warn("Wrong param " + param + " ; cannot be set :( "); 
+        this.update(); 
+        console.log(this);
+    }
+
+    updateInputs(){
+        let inputOffsetX = (this.node.params.w * this.node.factor) / (this.dpi * 2); 
+        let inputOffsetY = (this.node.params.h * this.node.factor) / (this.dpi * 2); 
+
+        this.wEl.style.top = `calc(50% - ${inputOffsetY + this.helpersSize}px)`;         
+        this.wEl.style.left = `calc(50% - ${inputOffsetX - this.node.params.w * (this.node.factor / this.dpi) / 2}px)`; 
+        this.hEl.style.top = `50%`; 
+        this.hEl.style.left = `calc(50% - ${inputOffsetX - (this.node.params.w / this.dpi) * this.node.factor - this.helpersSize}px`; 
+
+        this.helpers.forEach((helper, i) => {
+            if (helper.type == "input"){
+                let j = helper.index; 
+                if (helper.dir == "col"){
+                    this.colEl[j].style.left = `calc(50% - ${inputOffsetX - (this.helpers[i].pos + this.helpers[i].value / 2) * this.node.factor / 2}px)`; 
+                    this.colEl[j].style.top = `calc(50% - ${inputOffsetY + this.helpersSize / 2.2}px)`; 
+                }
+                if (helper.dir == "row"){
+                    this.rowEl[j].style.top = `calc(50% - ${inputOffsetY - (this.helpers[i].pos + this.helpers[i].value / 2) * this.node.factor / 2}px)`; 
+                    this.rowEl[j].style.left = `calc(50% + ${inputOffsetX + this.helpersSize / 2.4}px)`; 
+                }
+            }
+        })
+    }
+
+    initInputs(){
+        this.wEl.value = (this.node.params.w || 10); 
+        this.wEl.setAttribute("id", "we-width")
+        this.wEl.type = "number";
+        this.hEl.value = (this.node.params.h || 10); 
+        this.hEl.setAttribute("id", "we-height");
+        this.hEl.type = "number";
+        this.tip.classList.add("tooltip");
+        this.tip.style.display = "none"
+        this.root.appendChild(this.hEl);
+        this.root.appendChild(this.wEl);
+        this.root.appendChild(this.tip);
+        
+        this.colEl.forEach((el) => {el.remove()});
+        this.rowEl.forEach((el) => {el.remove()});
+
+        this.colEl = [];
+        this.rowEl = [];
+        this.node.params.row.forEach((row, i) => {
+            this.rowEl.push(document.createElement("input"));
+            this.rowEl[i].value = row;
+            this.rowEl[i].type = "number"; 
+            this.rowEl[i].setAttribute("id", "we-row-" + i);
+            this.rowEl[i].addEventListener("blur", () => {this.setRelief(this.rowEl[i])});
+            this.rowEl[i].addEventListener("change", (el) => {this.setParam("row-" + i, el.target.value)});
+
+            this.root.appendChild(this.rowEl[i]);
+        })
+        this.node.params.col.forEach((col, i) => {
+            this.colEl.push(document.createElement("input"));
+            this.colEl[i].value = col;
+            this.colEl[i].type = "number"; 
+            this.colEl[i].setAttribute("id", "we-col-" + i);
+            this.colEl[i].addEventListener("blur", () => {this.setRelief(this.colEl[i])});
+            this.colEl[i].addEventListener("change", (el) => {this.setParam("col-" + i, el.target.value)});
+
+            this.root.appendChild(this.colEl[i]);
+        })
+
+        this.hEl.addEventListener("blur", () => {this.setRelief(this.hEl)});
+        this.wEl.addEventListener("blur", () => {this.setRelief(this.wEl)});
+        this.hEl.addEventListener("change", (el) => {this.setParam("height", el.target.value)});
+        this.wEl.addEventListener("change", (el) => {this.setParam("width", el.target.value)});
     }
 
     calcHelpers(){
@@ -275,6 +432,7 @@ class WE{
                 helper.value =  (this.node.params.w - fixedSize) / 
                                 (this.node.params.colOrder.length - this.node.params.col.length);
             } else {
+                helper.index = this.node.params.colOrder[i]; 
                 helper.type = "input";
                 helper.value = this.node.params.col[order]; 
                 
@@ -282,6 +440,7 @@ class WE{
             helper.pos = 0; 
             for (let k = 0; k < i; k++) {
                 helper.pos += this.helpers[k].value; 
+                helper.inputPos = helper.pos + helper.value / 2;
             }
             this.helpers.push(helper); 
         })
@@ -293,12 +452,14 @@ class WE{
                 helper.value =  (this.node.params.h - fixedSize) / 
                                 (this.node.params.rowOrder.length - this.node.params.row.length);
             } else {
+                helper.index = this.node.params.rowOrder[i]; 
                 helper.type = "input";
                 helper.value = this.node.params.row[order]; 
             }
             helper.pos = 0; 
             for (let k = 0; k < i; k++) {
                 helper.pos += this.helpers[k + this.node.params.colOrder.length].value; 
+                helper.inputPos = helper.pos + helper.value / 2; 
             }
             this.helpers.push(helper); 
         })
@@ -310,10 +471,8 @@ class WE{
             this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height)
         }
 
-        this.ctx.font = `${4 * 8}px sans-serif`;
-        this.ctx.textAlign = "center";
         this.ctx.strokeStyle = "#000";
-        this.ctx.lineWidth = this.dpi; 
+        this.ctx.lineWidth = this.dpi * 1.5; 
         this.ctx.fillStyle = `hsl(${(node.bb.x2 / node.bb.y2) * 360 + node.bb.x1 * 3 }deg, 60%, 70%)`;
         this.ctx.fillRect(  node.geom.x1,
                             node.geom.y1,
@@ -333,6 +492,7 @@ class WE{
         this.canvas.height = this.canvas.clientHeight * this.dpi;
         this.node.factor = Math.min(    Math.floor(this.canvas.width / (this.node.params.w + 80)),
                                         Math.floor(this.canvas.height / (this.node.params.h + 80))) 
+        this.node.invFactor = 10 / this.node.factor; 
     }
 
     update(){
@@ -340,9 +500,10 @@ class WE{
         this.calcNode(this.node);
         this.scaleGeometry(this.node);
         this.calcOffset(this.node); 
-        this.drawNode(this.node); 
         this.calcHelpers();
+        this.drawNode(this.node); 
         this.drawHelpers();
+        this.updateInputs(); 
     }
 
     init(){
@@ -354,21 +515,34 @@ class WE{
             console.error("NO ROOT ELEMENT! Failed to init");
             return; 
         }
-        this.hEl.type = "number";
-        this.wEl.type = "number";
-        this.wEl.value = (this.node.params.w || 10); 
-        this.hEl.value = (this.node.params.h || 10); 
-        this.root.appendChild(this.canvas, this.hEl, this.wEl);
+        this.root.appendChild(this.canvas);
         this.canvas.width = this.canvas.clientWidth * this.dpi;
         this.canvas.height = this.canvas.clientHeight * this.dpi;
         this.node.factor = Math.min(    Math.floor(this.canvas.width / (this.node.params.w + 80)),
-                                        Math.floor(this.canvas.height / (this.node.params.h + 80))) 
+                                        Math.floor(this.canvas.height / (this.node.params.h + 80)));
+        this.node.invFactor = 10 / this.node.factor; 
         this.helpersSize *= this.node.factor; 
-        this.ctx = this.canvas.getContext("2d");  
+        this.ctx = this.canvas.getContext("2d"); 
+        if (!this.node.params.maxh)
+            this.node.params.maxh = 1000;
+        if (!this.node.params.minh)
+            this.node.params.minh = 10;
+        if (!this.node.params.maxw)
+            this.node.params.maxw = 1000;
+        if (!this.node.params.minw)
+            this.node.params.minw = 10;
+        if (!this.node.params.minCol)
+            this.node.params.minCol = [];
+        if (!this.node.params.maxCol)
+            this.node.params.maxCol = [];
+        if (!this.node.params.maxRow)
+            this.node.params.maxRow = [];
+        if (!this.node.params.minRow)
+            this.node.params.minRow = [];
+        this.initInputs(); 
         this.update();
     }
 }
-
 
 document.addEventListener("DOMContentLoaded", () => {
     let we = new WE("we-cont", win2); 
